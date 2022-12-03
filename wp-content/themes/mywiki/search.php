4a4fbd668303c269800d3f465c23e9aacc911827
mwhc00 <melvinwonghc@gmail.com>
2022-12-03 22:30:45 +0900

    Update search.php
    Fixed UI layout issue

--- wp-content/themes/mywiki/search.php
+++ wp-content/themes/mywiki/search.php
@@ -10,23 +10,30 @@
             </h4>
          </div>
       </header>
+	  <div style="padding-top: 30px;"></div>
+	  <div class="row">
+		<?php
+			add_filter('get_search_form', 'mywiki_search_form');
+            get_search_form('mywiki_search_form');
+            remove_filter('get_search_form', 'mywiki_search_form');
+		?>
+	  </div>
+	  <div style="padding-bottom: 30px;"></div>
+	  
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-        <header>
-            <div class="cat-hadding">
-                 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-            </div>
-            <p class="meta post-meta-entry"><?php mywiki_entry_meta(); ?></p>
-            
-        </header>
-        <!-- end article header -->
-        <section class="post_content">
-          <?php the_post_thumbnail( 'wpbs-featured' ); ?>
-          <?php the_excerpt(); ?>
-        </section>
-        <!-- end article section -->
-      </article>
-      <!-- end article -->
+		<hr class="hr hr-blurry" />
+		<div style="padding-top: 15px;"></div>
+		<div class="row">
+			<div class="col-md-2">
+			  <?php the_post_thumbnail( 'thumbnail' ); ?>
+			</div>
+				<h3 style="margin-top: 0px;" >
+					<a style="padding-left: 15px; -webkit-box-decoration-break: clone;" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+				</h3>
+			<div class="col-md-10" >
+				<?php the_excerpt(); ?>
+			</div>
+		</div>
       <?php endwhile; ?>
 	  <?php endif; ?>
 		<!--Pagination Start-->
@@ -39,8 +46,6 @@
     <!--Pagination End-->
     </div>
   </div>
-  <!-- end #main -->
-  <?php get_sidebar(); ?>
 </div>
 <!-- end #content -->
 <?php get_footer(); ?>
